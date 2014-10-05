eea1f361b5cb52f1bed58f585db586e6b03ee3ae
jparrish832 <jparrish832@gmail.com>
2014-10-05 14:02:58 -0400

    Add test code

---
+++ test-src/com/staticvillage/data/test/CaptureClientTest.java
@@ -0,0 +1,35 @@
+package com.staticvillage.data.test;
+
+import java.io.IOException;
+import java.net.UnknownHostException;
+
+import com.staticvillage.client.data.AccelerometerData;
+import com.staticvillage.client.data.CaptureClient;
+
+public class CaptureClientTest {
+
+	public static void main(String[] args) {
+		CaptureClient client = new CaptureClient("staticvillageappid");
+		
+		try {
+			client.connect();
+			
+			for(int i=0;i<10;i++){
+				//pulling accelermoter from wrong prject fix
+				AccelerometerData a = new AccelerometerData();
+				a.x   		 = 12.345*i;
+				a.y			 = 1.2*i;
+				a.z			 = 0.03412*i;
+				
+				client.report(a);
+			}
+			
+			client.close();
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
